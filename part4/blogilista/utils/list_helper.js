1686c1cadba8f023a968effdb719195ec32750a9
Kärhä Erno <ekarha@lapinamk.fi>
2021-03-10 22:06:05 +0200

    Did some testing tasks in part 4

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -1,7 +1,30 @@
 const dummy = (blogs) => {
-  // ...
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+
+  const testi = blogs.map(blogs => blogs.likes)
+
+  return testi.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((accumulator, current) => (accumulator.likes > current.likes) ? accumulator : current)
+}
+
+
+const mostBlogs = (blogs) => {
+
+  // Todo
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
