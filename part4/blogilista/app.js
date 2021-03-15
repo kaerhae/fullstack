059fa5fb5dc49b6adb0679f1e8806a97f58e4cc0
kaerhae <erno.karha@hotmail.com>
2021-03-15 02:10:53 +0200

    Did tasks 4b

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -1,7 +1,9 @@
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
+require('express-async-errors')
 const cors = require('cors')
+const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -18,11 +20,11 @@ mongoose.connect(config.MONGODB, { useNewUrlParser: true, useUnifiedTopology: tr
   })
 
 app.use(cors())
-app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,23 +1,64 @@
-const blogsRouter = require('express').Router()
+const { request, response } = require('express')
+const express = require('express')
 const Blog = require('../models/blog')
+const blogsRouter = express.Router()
 
-blogsRouter.get('/', (response) => {
-  Blog
-    .find({})
-    .then(res => {
-      response.json(res.map(blog => blog.toJSON()))
-    })
+blogsRouter.get('/', async (request, response) => {
+  
+  const blogs = await Blog.find({})
+  response.json(blogs.map(blog => blog.toJSON()))
+  
+  
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(res => {
-      response.status(201).json(res)
-    })
-    .catch(error => next(error))
+blogsRouter.get('/:id', async (request, response, next) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog.toJSON())
+    } else {
+      response.status(404).end()
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+
+})
+
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  if (body.title === undefined) {
+    return response.status(400).json({ error: 'Title missing'})
+  }
+
+  if (body.author === undefined) {
+    return response.status(400).json({ error: "Author name missing"})
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes === undefined ? 0 : body.likes,
+  })
+  const savedBlog = await blog.save()
+  response.json(savedBlog.toJSON())
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true})
+  response.json(updatedBlog.toJSON())
 })
 
 module.exports = blogsRouter
